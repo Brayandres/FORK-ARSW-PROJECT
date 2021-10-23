2a6602edd0e77b62a582dacc025fc074fa1219ea
Brayan Macias <brayan.macias@escuelaing.edu.co>
2021-10-23 16:43:06 -0500

    Context added

--- src/main/java/edu/eci/arsw/projectapi/ProjectAPIApplication.java
+++ src/main/java/edu/eci/arsw/projectapi/ProjectAPIApplication.java
@@ -2,13 +2,16 @@ package edu.eci.arsw.projectapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"edu.eci.arsw.project"})
 public class ProjectAPIApplication {
 
     public static void main(String[] args) {
+    	ApplicationContext context = new FileSystemXmlApplicationContext();
         SpringApplication.run(ProjectAPIApplication.class, args);
     }
 }
